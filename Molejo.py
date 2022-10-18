132d1bd6281d7496908f4fe24f8034418e88a5f3
Allysson <58224574+Allyss0n@users.noreply.github.com>
2022-10-18 15:42:40 -0300

    Versão 2
    Versão para teste de velocidade de processamento, sem a etapa do menu

--- Molejo.py
+++ Molejo.py
@@ -1,192 +1,231 @@
 import numpy as np
+from itertools import combinations
+from timeit import default_timer as timer
+import csv
 
 
 class Molejo:
 
-    def __init__(self, numberOfMatrices):
+    def __init__(self, numberOfMatrices, runAllOperations=True):
 
-        self.operationCalledTimes = 0
-        self.matrices = []
-        self.initialMatrices = []
-        self.result = []
+        if runAllOperations:
+
+            self.runAllOperations(numberOfMatrices)
+        else:
+            self.matrices = self.createMatrices(numberOfMatrices)
+
+            self.chooseOperation()
+
+    # testes para ganho de perfomance
+    def runAllOperations(self, numberOfMatrices):
+
+        start = timer()
+        sumResult = []
+        subtractionResult = []
+        productResult = []
+        determinantResult = []
+        inverseResult = []
+
+        MATRICES = self.createMatrices(numberOfMatrices)
+
+        ALL_COMBINATIONS = combinations(MATRICES, 2)
+
+        for i in ALL_COMBINATIONS:
+
+            A = i[0]
+            B = i[1]
+
+            shapeA = np.shape(A)
+            shapeB = np.shape(B)
+
+            if (shapeA == shapeB):
+                sumResult.append(A+B)
+                subtractionResult.append(A-B)
+
+            if (shapeA[1] == shapeB[0]):
+                productResult.append(np.dot(A, B))
+
+        for i in MATRICES:
+
+            shape = np.shape(i)
+
+            if (shape[0] == shape[1]):
+                det = round(np.linalg.det(i))
+                determinantResult.append(det)
+
+                if det != 0:
+                    inverseResult.append(np.linalg.inv(i))
+
+        if numberOfMatrices < 20:
+            with open('sum.csv', 'w', newline='') as file:
+                mywriter = csv.writer(file, delimiter=',')
+                mywriter.writerows(sumResult)
+            with open('subtraction.csv', 'w', newline='') as file:
+                mywriter = csv.writer(file, delimiter=',')
+                mywriter.writerows(subtractionResult)
+            with open('product.csv', 'w', newline='') as file:
+                mywriter = csv.writer(file, delimiter=',')
+                mywriter.writerows(productResult)
+            with open('determinant.csv', 'w', newline='') as file:
+                mywriter = csv.writer(file, delimiter='\n')
+                mywriter.writerow(determinantResult)
+            with open('inverse.csv', 'w', newline='') as file:
+                mywriter = csv.writer(file, delimiter=',')
+                mywriter.writerows(inverseResult)
+        else:
+            np.savetxt('sum.csv', sumResult, delimiter=',',
+                       fmt='%s', newline='\n\n')
+            np.savetxt('subtraction.csv', subtractionResult,
+                       delimiter=',', fmt='%s', newline='\n\n')
+            np.savetxt('product.csv', productResult,
+                       delimiter=',', fmt='%s', newline='\n\n')
+            np.savetxt('determinant.csv', determinantResult,
+                       delimiter=',', fmt='%s', newline='\n\n')
+            np.savetxt('inverse.csv', inverseResult,
+                       delimiter=',', fmt='%s', newline='\n\n')
+
+        end = timer()
+
+        print(
+            f'\n\n\n\n\n# All done!\n\n# It tooked {(end-start)} seconds to complete all the operations.')
+
+    def chooseOperation(self):
+
+        options = {1: 'sum', 2: 'subtraction', 3: 'multiplication',
+                   4: 'determinant', 5: 'inverse'}
+
+        chosenOption = int(input(
+            '\n\n# Now enter the number of the operation you want to perform:\n   1. Sum\n   2. Subtraction\n   3. Multiplication\n   4. Determinant\n   5. Inverse\n   6. Exit\n\n> '))
+
+        if chosenOption == 6:
+            print('Exiting the program then...')
+            exit()
+        elif chosenOption in options:
+            self.operate(options[chosenOption])
+        else:
+            print('Didn\'t understand, try again...')
+            self.chooseOperation()
+
+    def createMatrices(self, numberOfMatrices):
+
+        matrices = []
 
         i = 0
-        m = np.random.randint(2, 5)
-        n = m
 
         while i < numberOfMatrices:
+            m = np.random.randint(2, 5)
+            n = np.random.randint(2, 5)
 
-            matrix = self.createMatrix(m, n)
-            self.matrices.append(matrix)
-            self.initialMatrices.append(matrix)
+            matrices.append(np.random.randint(-99, 99, size=(m, n)))
 
             i += 1
 
-    def createMatrix(self, m, n):
-        return np.random.randint(-99, 99, size=(m, n))
+        return matrices
 
     def operate(self, operation):
 
-        self.currentOperation = operation
+        currentOperation = operation
 
         operation = getattr(self, operation)
 
-        if self.validateOperation():
+        result = operation()
 
-            operation()
+        self.toCsv(currentOperation, result)
 
-            self.toCsv()
-
-            return self.result
-
-        else:
-            print(
-                'The operation couldn\'t be completed because it didn\'t pass the validation.')
+        print(result)
 
     def sum(self):
 
-        # a = self.matrices.pop(0)
-        # b = self.matrices.pop(0)
+        allCombinations = combinations(self.matrices, 2)
 
-        # self.matrices.insert(0, a+b)
+        result = []
 
-        while len(self.matrices) >= 2:
-            a = self.matrices.pop(0)
-            b = self.matrices.pop(0)
-            self.matrices.insert(0, a+b)
+        for i in allCombinations:
 
-        self.result = self.matrices
-        # print('\n\n#result:\n  ', self.matrices)
+            A = i[0]
+            B = i[1]
+
+            shapeA = np.shape(A)
+            shapeB = np.shape(B)
+
+            if (shapeA == shapeB):
+                result.append(A+B)
+
+        return result
 
     def subtraction(self):
 
-        self.operationCalledTimes = (self.operationCalledTimes + 1)
+        allCombinations = combinations(self.matrices, 2)
 
-        a = self.matrices.pop(0)
-        b = self.matrices.pop(0)
+        result = []
 
-        self.matrices.insert(0, a-b)
+        for i in allCombinations:
 
-        if len(self.matrices) >= 2:
-            self.subtraction()
-        else:
-            self.result = self.matrices
-            # print('\n\n#result:\n  ', self.matrices)
+            A = i[0]
+            B = i[1]
+
+            shapeA = np.shape(A)
+            shapeB = np.shape(B)
+
+            if (shapeA == shapeB):
+                result.append(A-B)
+
+        return result
 
     def multiplication(self):
 
-        self.operationCalledTimes = (self.operationCalledTimes + 1)
+        allCombinations = combinations(self.matrices, 2)
 
-        a = self.matrices.pop(0)
-        b = self.matrices.pop(0)
+        result = []
 
-        self.matrices.insert(0, np.dot(a, b))
+        for i in allCombinations:
 
-        if len(self.matrices) >= 2:
-            self.multiplication()
-        else:
-            self.result = self.matrices
-            # print('\n\n#result:\n  ', self.matrices)
+            A = i[0]
+            B = i[1]
+
+            shapeA = np.shape(A)
+            shapeB = np.shape(B)
+
+            if (shapeA[1] == shapeB[0]):
+                result.append(np.dot(A, B))
+
+        return result
 
     def determinant(self):
 
-        self.operationCalledTimes = (self.operationCalledTimes + 1)
+        result = []
 
-        length = len(self.matrices)
+        for i in self.matrices:
 
-        i = 0
+            shape = np.shape(i)
 
-        while i < length:
+            if (shape[0] == shape[1]):
+                det = round(np.linalg.det(i))
+                result.append(det)
 
-            a = self.matrices.pop(i)
-            det = np.linalg.det(a)
-            self.matrices.insert(i, round(det))
-            i = i + 1
-
-        self.result = self.matrices
-        # print('\n\n#result:\n  ', self.matrices)
+        return result
 
     def inverse(self):
 
-        self.operationCalledTimes = (self.operationCalledTimes + 1)
+        result = []
 
-        length = len(self.matrices)
+        determinants = self.determinant()
+
+        length = len(determinants)
 
         i = 0
 
         while i < length:
 
-            a = self.matrices.pop(i)
-            inv = np.linalg.inv(a)
-            self.matrices.insert(i, inv)
-            i = i + 1
+            shape = np.shape(self.matrices[i])
 
-        self.result = self.matrices
-        # print('\n\n#result:\n  ', self.matrices)
+            if (shape[0] == shape[1]) and (determinants[i] != 0):
+                result.append(np.linalg.inv(self.matrices[i]))
 
-    def toCsv(self):
+            i += 1
 
-        if self.currentOperation == 'determinant':
-            np.savetxt(self.currentOperation + ".csv",
-                       self.result, delimiter=",")
-        else:
-            for i in self.result:
-                np.savetxt(self.currentOperation +
-                           ".csv", i, delimiter="   ")
+        return result
 
-    def validateOperation(self):
-
-        if self.currentOperation == 'sum' or self.currentOperation == 'subtraction':
-
-            lengnth = len(self.matrices)
-
-            if lengnth < 2:
-                return False
-
-            i = 0
-
-            while i < (lengnth - 1):
-
-                if (np.shape(self.matrices[i]) != np.shape(self.matrices[i+1])):
-                    return False
-                i += 1
-
-            return True
-
-        elif self.currentOperation == 'multiplication':
-
-            lengnth = len(self.matrices)
-
-            if lengnth < 2:
-                return False
-
-            i = 0
-
-            while i < (lengnth - 1):
-                if (np.shape(self.matrices[i])[1] != np.shape(self.matrices[i+1])[0]):
-                    return False
-                i += 1
-
-            return True
-
-        elif self.currentOperation == 'determinant':
-
-            for i in self.matrices:
-
-                shape = np.shape(i)
-
-                if (shape[0] != shape[1]):
-                    return False
-
-            return True
-
-        elif self.currentOperation == 'inverse':
-
-            self.determinant()
-
-            if 0 in self.result:
-                return False
-
-            self.matrices = self.initialMatrices
-
-            return True
+    def toCsv(self, currentOperation, result):
+        np.savetxt(currentOperation + '.csv', result,
+                   delimiter=',', fmt='%s', newline='\n\n')
